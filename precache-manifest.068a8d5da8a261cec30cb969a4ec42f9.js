e6e8a488df8b60ea6b1dbe79ef4d1ec9fe1aefcc
Angel Pan <angelpan@Angels-MacBook-Pro.local>
2020-09-29 18:33:03 -0700

    Updates

---
+++ precache-manifest.068a8d5da8a261cec30cb969a4ec42f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b655efdac8d5d8406152e5c9c67f7c4f",
+    "url": "/werkcal/index.html"
+  },
+  {
+    "revision": "333281192b526b79a8aa",
+    "url": "/werkcal/static/css/2.b3bd945d.chunk.css"
+  },
+  {
+    "revision": "3ef679e390e1ae30e89e",
+    "url": "/werkcal/static/css/main.94377fc7.chunk.css"
+  },
+  {
+    "revision": "333281192b526b79a8aa",
+    "url": "/werkcal/static/js/2.2b1a9692.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/werkcal/static/js/2.2b1a9692.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ef679e390e1ae30e89e",
+    "url": "/werkcal/static/js/main.46c52fbc.chunk.js"
+  },
+  {
+    "revision": "85913574b798b469a912",
+    "url": "/werkcal/static/js/runtime-main.64344efe.js"
+  }
+]);
